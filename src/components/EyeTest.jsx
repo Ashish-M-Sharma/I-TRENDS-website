1ad742f00023a980c5098e44b8b23cfbf3d61ec4
Ashish-M-Sharma <a.m.sharma2501@gmail.com>
2025-02-17 02:01:37 +0530

    make a change

--- src/components/EyeTest.jsx
+++ src/components/EyeTest.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const EyeTest = () => {
   return (
     <>
-      <div className=" w-full flex justify-center items-center gap-5 mt-13 my-10 ">
-        <div className="border w-[550px] h-[1.5px] border-zinc-400"></div>
-        <p className="text-[30px] font-[700] leading-[42px]">
+      <div className="w-full flex flex-col md:flex-row justify-center items-center gap-5 mt-13 my-10 text-center">
+        <div className="border w-full md:w-[200px] lg:w-[550px] h-[1.5px] border-zinc-400"></div>
+        <p className="text-[20px] md:text-[24px] lg:text-[30px] font-[700] leading-[30px] md:leading-[36px] lg:leading-[42px]">
           FREE ONLINE EYE TEST
         </p>
-        <div className="border w-[550px] h-[1.5px] border-zinc-400"></div>
+        <div className="border w-full md:w-[200px] lg:w-[550px] h-[1.5px] border-zinc-400"></div>
       </div>
 
       <div className=" w-full h-auto">
